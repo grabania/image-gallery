b8c3727c3a08fa37db578dc0d26bce87793b5565
grabania <grabania.work@gmail.com>
2020-07-05 23:28:04 +0100

    Add toggleActive function in JS

--- script.js
+++ script.js
@@ -5,4 +5,12 @@ function toggleOpen() {
 	this.classList.toggle('open');
 }
 
+function toggleActive(e) {
+	console.log(e.propertyName);
+	if (e.propertyName.includes('flex')) {
+		this.classList.toggle('open-active');
+	}
+}
+
 panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+panels.forEach((panel) => panel.addEventListener('transitionend', toggleActive));
